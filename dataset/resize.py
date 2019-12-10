c1f04d173f660d80c1c322fb8feebabf304d91c4
Aleksandar Aleksandrov <aaleksandar.aleksandrov@gmail.com>
2019-12-10 16:49:40 +0100

    first batch

---
+++ dataset/resize.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+import sys
+from pathlib import Path
+
+dir = Path(__file__).parent.joinpath('images/beans')
+
+for file_name in dir.iterdir():
+    print("Processing %s" % file_name)
+    image = Image.open(file_name)
+
+    output = image.resize((224, 224), Image.ANTIALIAS)
+
+    #output_file_name = os.path.join(dir, "small_" + file_name)
+    output.save(file_name, "PNG", quality = 95)
+
+    print("All done")
