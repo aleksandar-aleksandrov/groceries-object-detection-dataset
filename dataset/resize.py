7012804174364da96701200956c0fd19f6a2a700
Aleksandar Aleksandrov <aaleksandar.aleksandrov@gmail.com>
2019-12-13 00:24:54 +0100

    Added images for flour

--- dataset/resize.py
+++ dataset/resize.py
@@ -3,7 +3,7 @@ import os
 import sys
 from pathlib import Path
 
-dir = Path(__file__).parent.joinpath('images/fish')
+dir = Path(__file__).parent.joinpath('images/flour')
 
 for file_name in dir.iterdir():
     print("Processing %s" % file_name)
