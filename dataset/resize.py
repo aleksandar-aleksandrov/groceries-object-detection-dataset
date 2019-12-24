3f5dd771f0f5576fb8ec52f0b8adbc431ba8eed6
Aleksandar Aleksandrov <aaleksandar.aleksandrov@gmail.com>
2019-12-24 14:20:37 +0100

    Added notations and images for new classes

--- dataset/resize.py
+++ dataset/resize.py
@@ -3,7 +3,7 @@ import os
 import sys
 from pathlib import Path
 
-dir = Path(__file__).parent.joinpath('images/flour')
+dir = Path(__file__).parent.joinpath('images/nuts')
 
 for file_name in dir.iterdir():
     print("Processing %s" % file_name)
